a261ffb96b0813e7d49da65e4c041e5125de92ca
dagechan <takebose5@gmail.com>
2024-05-02 13:47:34 +0900

    マージ機能の追加とpptxファイルの追加

--- git-visual/public/client.js
+++ git-visual/public/client.js
@@ -1,11 +1,11 @@
-// fetchCommits 関数を定義
+// commit情報を取得する
 async function fetchCommits() {
     const response = await fetch('http://localhost:3000/commits');
     const data = await response.json();
     return data.commits;
 }
 
-// drawGitGraph 関数を定義
+// commitグラフの描画
 async function drawGitGraph() {
     const commits = await fetchCommits(); // fetchCommits 関数を呼び出し
 
@@ -13,21 +13,43 @@ async function drawGitGraph() {
     const gitgraph = GitgraphJS.createGitgraph(graphContainer);
 
     const branches = {};
+    var pre_branchName = ""; // マージ元のブランチ名を保持する変数
 
     commits.forEach(commit => {
-        const branchName = commit.branch.split(',')[0].trim(); // ブランチ名を取得
+        console.log("mergeParentBranchは"+commit.mergeParentBranch); // デバッグ用ログ
+        console.log(branches); // デバッグ用ログ
+        
 
-        if (!branches[branchName]) {
+        const branchName = commit.branch.split(',')[0].trim(); // ブランチ名を取得
+        /* マージコミットのときは，branchNameは空文字になる */
+        console.log("branchNameは"+branchName);
+
+        if (!branches[branchName] && branchName !== "") {
             branches[branchName] = gitgraph.branch(branchName); // ブランチを作成
         }
 
-        branches[branchName].commit({
-            subject: commit.message,
-            author: commit.author,
-            hash: commit.commit,
-            date: new Date(commit.date)
-        });
+        if (commit.parents.length > 1) {
+            // マージコミットの場合の処理
+            console.log("merge commit");
+            const mergeMessage = `Merge branch: ${commit.mergeParentBranch}`;
+            console.log(pre_branchName);
+            console.log(branches[pre_branchName]);
+            branches[commit.mergeParentBranch].merge({ branch: branches[pre_branchName], message: mergeMessage }); // マージ元のブランチ名を指定してマージ
+        } else {
+            // マージコミットでない場合の処理
+            console.log("normal commit");
+            branches[branchName].commit({
+                subject: commit.message,
+                author: commit.author,
+                hash: commit.commit,
+                date: new Date(commit.date)
+            });
+        }
+        pre_branchName = branchName; // マージ元のブランチ名を更新
+        console.log("pre_branchNameは"+pre_branchName); // デバッグ用ログ
+        console.log("-----------------------------------");
     });
+
 }
 
 drawGitGraph(); // drawGitGraph 関数を呼び出し
