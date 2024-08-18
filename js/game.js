11d81d0db7b74c1514bef493ceef6da896964982
dagechan <takebose5@gmail.com>
2024-08-18 22:00:52 +0900

    add quiz-scene

--- js/game.js
+++ js/game.js
@@ -1,6 +1,8 @@
+// このシステムで使うシーンをインポート
 import { TitleScene } from './scenes/TitleScene.js';
 import { MainGameScene } from './scenes/MainGameScene.js';
 import { DiscussionScene } from './scenes/DiscussionScene.js';
+import { QuizScene } from './scenes/QuizScene.js';
 
 // ゲームの設定
 const config = {
@@ -9,7 +11,7 @@ const config = {
     height: window.innerHeight,
     backgroundColor: '#024578',
     parent: 'game-container',
-    scene: [TitleScene, MainGameScene, DiscussionScene],  // シーンを設定
+    scene: [TitleScene, MainGameScene, DiscussionScene, QuizScene],  // インポートしたシーンを設定
     physics: {
         default: 'arcade',
         arcade: {
--- js/scenes/QuizScene.js
+++ js/scenes/QuizScene.js
@@ -1,19 +1,21 @@
-export class DiscussionScene extends Phaser.Scene {
+export class QuizScene extends Phaser.Scene {
     constructor() {
-        super({ key: 'DiscussionScene' });
-        this.timerDuration = 10; // タイマーの時間（秒）
+        super({ key: 'QuizScene' });
+        this.timerDuration = 60; // タイマーの時間（秒）
     }
 
     preload() {
-        this.load.image('dummy-button', '../../assets/images/dummy-button.png');
     }
 
     create() {
         // ディスカッション開始のメッセージ表示
-        this.messageText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, 'Discussion Time!', {
+        this.messageText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, '発生した障害の種類を以下から選んでください', {
             fontSize: '32px',
             fill: '#000'
         }).setOrigin(0.5);
+
+        // 選択肢を表示する
+        this.createQuizButtons();
 
         // タイマーを表示するテキスト
         this.timerText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + 50, `Time Left: ${this.timerDuration}`, {
@@ -24,6 +26,22 @@ export class DiscussionScene extends Phaser.Scene {
         // タイマーを開始する
         this.startTimer();
 
+    }
+
+    createQuizButtons() {
+        const buttonScale = 0.5;
+        const buttonWidth = this.textures.get('dummy-button').getSourceImage().width * buttonScale;
+        const buttonHeight = this.textures.get('dummy-button').getSourceImage().height * buttonScale;
+        
+        const x = this.cameras.main.width - buttonWidth / 2 - 200;
+        const y = this.cameras.main.height - buttonHeight / 2 - 200;
+
+        this.add.image(x, y, 'dummy-button')
+            .setInteractive()
+            .setScale(buttonScale)
+            .on('pointerdown', () => {
+                this.scene.start('RepairScene');
+            });
     }
     
     startTimer() {
@@ -43,7 +61,7 @@ export class DiscussionScene extends Phaser.Scene {
         if (this.timerDuration <= 0) {
             // タイマーがゼロになったら次のシーンへ移動
             this.timeEvent.remove(); // タイマーイベントを停止
-            this.scene.start('QuizScene'); // 'MainGameScene'に移動
+            this.scene.start('MainGameScene'); // 'MainGameScene'に移動
         }
     }
 
